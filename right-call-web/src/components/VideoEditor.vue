a4cc10a7103dc3974e6a0ea89ed25bfc5fc4b892
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2022-10-16 13:08:20 -0400

    add functionality of videos stopping at timestamps

--- right-call-web/src/components/VideoEditor.vue
+++ right-call-web/src/components/VideoEditor.vue
@@ -1,27 +1,47 @@
 <template>
-    <div>
-        <h1>{{msg}}</h1>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <video id="myVideoPlayer" src="../assets/prototype_withAudio.webm" controls />
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'VideoEditor',
-    props: {
-      msg: String
-    }
+  <div>
+      <h1>{{msg}}</h1>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <video id="myVideoPlayer" src="../assets/prototype_withAudio.webm" controls />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoEditor',
+  props: {
+    msg: String
+  },
+  mounted() {
+    const video = document.getElementById('myVideoPlayer');
+    let timestamps = [3.7, 14.2, 30.3]   //make an array out of provided sample timestamps
+
+    
+    video.addEventListener('timeupdate', () => {    //listen for when the video's time changes
+      stopVideoAtTimestamp(video, timestamps)
+    })
+    
+    
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  video {
-    width: 45%;
+}
+
+function stopVideoAtTimestamp(video, timestamps) {
+
+  var currentTime = video.currentTime;
+  if (currentTime >= timestamps[0]) {
+    timestamps.shift()   // remove first timestamp
+    video.pause();
   }
-  </style>
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+video {
+  width: 45%;
+}
+</style>
 
 
-  
