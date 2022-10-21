39d93b1fcc4bf56be4ce06b8db760bafc6cbad2d
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2022-10-20 20:30:18 -0400

    Make VideoEditor ready to accept VideoClip object

--- right-call-web/src/App.vue
+++ right-call-web/src/App.vue
@@ -3,23 +3,37 @@
     <NavBar />
     <div class="container">
       <div class="video-container">
-        <VideoEditor msg="RefReps Video Editor" />
+        <!-- LessonSelection goes here -->
+        <!-- This VideoEditor line below will need to be moved to the LessonSelection page, because that's where the video will be slected and VideoClop object created -->
+        <VideoEditor v-if="ready" :video="sampleVideo" title="RefReps Video Editor" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-/*import HelloWorld from './components/HelloWorld.vue'*/
 import NavBar from "./components/Navbar.vue";
 import VideoEditor from './components/VideoEditor.vue'
+import VideoClip from "@/models/VideoClip.js"
 
 export default {
   name: 'App',
   components: {
     NavBar,
     VideoEditor
+  },
+  // With the lesson selection page, when the user clicks on a lesson, create a videoClip object and pass that as a prop to the videoEditor component.
+  // It currently works right now, but this data() and mounted() should be deleted from this file when lessonSelection file is created,
+  // because I created a sample VideoClip object in this file, but it will be created in the LessonSelection page
+  data() {
+    return {
+      sampleVideo: VideoClip,
+      ready: false,
+    }
+  },
+  mounted() {
+    this.sampleVideo = new VideoClip("sampleVid", 125, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4", [3.7, 14.2, 30.3])
+    this.ready = true;
   }
 }
 </script>
