96d64c695d53ee8297262e89b7bb2c2e0889eaa4
Lyle Dotson <ld77668952@gmail.com>
2022-10-21 15:32:34 -0400

    Create LessonSelection.vue Add LessonSelection to App.Vue

---
+++ right-call-web/src/components/LessonSelection.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+import "../assets/prototype_withAudio.webm"
+// import VideoClip from "@/models/VideoClip.js"
+//@TODO This page must display a list of links of lessons
+//    which would have a video object .
+//The user picked lesson to VideoEditor .
+
+export default {
+  name: 'VideoEditor',
+  props: {
+    title: String
+  }
+}   
+</script>
