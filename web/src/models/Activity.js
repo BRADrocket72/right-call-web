6271ec8a431ef48653b8237f3c531d8cc44ddb09
Joshua Neumann <janeumann@bsu.edu>
2022-10-27 16:21:19 -0400

    Add Activity.js

---
+++ web/src/models/Activity.js
@@ -0,0 +1,7 @@
+export default class Activity {
+    constructor(id, questionText, attribute) {
+        this.id = id;
+        this.questionText = questionText;
+        this.attribute = attribute;
+    }
+}
