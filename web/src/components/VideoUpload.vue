f8534e9650a049c8f91c5b87ebcb81ece914a9d2
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2022-11-14 19:21:21 -0500

    fix mistake

--- web/src/components/VideoUpload.vue
+++ web/src/components/VideoUpload.vue
@@ -1,8 +1,5 @@
 <template>
     <br>
     <label><h4>Upload Video File:</h4></label>
-    <input type="file" id="videoUpload" name="videouUpload" />
-    <br><br>
-    <label><h4>Or Enter Url of Video:</h4></label>
-    <input type="text" id="urlUpload" name="urlUpload" />
+    <input type="file" id="videoUpload" name="videoUpload" />
 </template>
