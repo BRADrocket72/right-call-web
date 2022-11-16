c7aa0fcc6691a686d6bbbbd3bba060f679c4465b
lleckron <leckmanbaseball@gmail.com>
2022-11-15 19:20:08 -0500

    create modal popup for AssignTimestamp

---
+++ web/src/components/AssignTimestampsModal.vue
@@ -0,0 +1,116 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div class="modal" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
+        <header class="modal-header" id="modalTitle">
+          <slot name="header">
+            Lesson Results
+          </slot>
+        </header>
+
+        <section class="modal-body" id="modalDescription">
+          <slot name="body">
+            <p>{{this.percentageCorrect}} Correct</p>
+            <br>
+            <p v-for="n in this.eachQuestionResults.length" :key="n">Question {{n}}: {{this.eachQuestionResults[n-1]}}
+            </p>
+
+            <button type="button" class="btn-green" @click="close()"> Back to Lesson Selection Page </button>
+          </slot>
+        </section>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+
+export default {
+  name: 'ResultsPage',
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: white;
+  width: 600px;
+  height: 300px;
+  border-radius: 2px;
+  position: fixed;
+  left: auto;
+  top: auto;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header {
+  position: relative;
+  border-bottom: 1px solid #eeeeee;
+  background-color: #4AAE9B;
+  justify-content: space-between;
+  border-radius: 2px 2px 0px 0px;
+
+}
+
+.modal-footer {
+  border-top: 1px solid #eeeeee;
+  flex-direction: column;
+}
+
+.modal-body {
+  position: relative;
+  background: white;
+  padding: 20px 10px;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4AAE9B;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4AAE9B;
+  border: 1px solid #eeeeee;
+  border-radius: 2px;
+  padding: 15px;
+  margin: 10px;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity .5s ease;
+}
+</style>
