13b27dca6d73e7871c7ca977b23c2ce046d7eaa6
lleckron <leckmanbaseball@gmail.com>
2022-11-16 01:03:16 -0500

    revert emit commit, unnecessary change

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,10 +3,5 @@ import "bootstrap/dist/js/bootstrap.js"
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './routes'
-import mitt from 'mitt'
-const emitter = mitt();
-export default emitter;
-const app = createApp(App)
-app.config.globalProperties.emitter = emitter;
 
-app.use(Router).mount('#app')
+createApp(App).use(Router).mount('#app')
