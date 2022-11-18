3d2da3ec94792485c94ffb7cbe43eb3826499889
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2022-11-18 13:52:02 -0500

    enabled cors on the api

--- api/index.js
+++ api/index.js
@@ -22,6 +22,11 @@ app.use(express.json());
 
 app.use('/api', routes)
 
+const cors = require('cors');
+app.use(cors({
+    origin: ['*']
+}));
+
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
