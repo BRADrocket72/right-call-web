481f549ee4251a6e71e571b65f2f63e5b9849c50
Lyle Dotson <ld77668952@gmail.com>
2022-11-27 10:42:04 -0500

    Add Lesson.js to Api

---
+++ api/models/Lessons.js
@@ -0,0 +1,8 @@
+export default class Lesson {
+    id = "";
+    name = "";
+    description = "";
+    constructor(id, name, description){
+        this.id = id;
+        this.name = name,
+        this.description = description }}
