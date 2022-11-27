630912b5c6f3fb8ef3e7d4bfd0615b3f60052691
Lyle Dotson <ld77668952@gmail.com>
2022-11-27 10:49:43 -0500

    Add Lesson.js to Api

--- api/models/Lessons.js
+++ api/models/Lessons.js
@@ -1,8 +1,21 @@
-export default class Lesson {
-    id = "";
-    name = "";
-    description = "";
-    constructor(id, name, description){
-        this.id = id;
-        this.name = name,
-        this.description = description }}
+const mongoose = require('mongoose');
+
+const lessonSchema = new mongoose.Schema({
+    // name of the lesson
+    name: {
+        required: true,
+        type: String,
+    },
+    // descriptions of the lessons 
+    description: {
+        required: true,
+        type: String
+    },
+    // array of the video clips
+    videoClipsArray: {
+        required: true,
+        type: Array
+    }
+})
+
+module.exports = mongoose.model('Lessons', lessonSchema)
