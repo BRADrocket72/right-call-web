ca00ea3ad174ccad5ba027c4e2e15f31f35e8e3e
lleckron <leckronluke@gmail.com>
2022-12-02 14:14:35 -0500

    initial assignTimestamps tests skeleton

---
+++ web/tests/unit/AssignTimestamps.spec.ts
@@ -0,0 +1,15 @@
+import { mount } from '@vue/test-utils'
+import AssignTimestamps from '@/components/AssignTimestamps.vue'
+import AssignTimestampsModal from '@/components/modals/AssignTimestampsModal.vue'
+import AssignActivityModal from '@/components/modals/AssignActivityModal.vue'
+import SaveTimestampsModal from '@/components/modals/SaveTimestampsModal.vue'
+import 'jest'
+
+describe('LessonSelection.vue', () => {
+    let wrapper: any;
+    beforeEach(() => {
+        wrapper = mount(AssignTimestamps)
+    })
+
+    
+})
