2a2c9f5a2c864abe3a76b5fb82c3c138e2935b3f
lleckron <leckronluke@gmail.com>
2022-12-03 09:55:22 -0500

    preliminary commit of skeleton

--- web/tests/unit/AssignTimestamps.spec.ts
+++ web/tests/unit/AssignTimestamps.spec.ts
@@ -1,15 +1,52 @@
 import { mount } from '@vue/test-utils'
+import { setActivePinia, createPinia } from 'pinia'
+import { useVideoClipStore } from "@/stores/VideoClipStore"
+import { useActivityStore } from '@/stores/ActivityStore'
+import { useUsersStore } from '@/stores/UserStore'
 import AssignTimestamps from '@/components/AssignTimestamps.vue'
-import AssignTimestampsModal from '@/components/modals/AssignTimestampsModal.vue'
-import AssignActivityModal from '@/components/modals/AssignActivityModal.vue'
-import SaveTimestampsModal from '@/components/modals/SaveTimestampsModal.vue'
+//import AssignTimestampsModal from '@/components/modals/AssignTimestampsModal.vue'
+//import AssignActivityModal from '@/components/modals/AssignActivityModal.vue'
+//import SaveTimestampsModal from '@/components/modals/SaveTimestampsModal.vue'
 import 'jest'
 
-describe('LessonSelection.vue', () => {
-    let wrapper: any;
-    beforeEach(() => {
-        wrapper = mount(AssignTimestamps)
+describe('AssignTimestamps.vue', () => {
+    let wrapper: any
+    //let videoStore = null
+    //let activityStore = null
+    //let retrieveAllVideos: any
+    //let retrieveSpecificVideo: any
+    beforeAll(async () => {
+        setActivePinia(createPinia())
+        
+        // create an instance of the data stores
+        //videoStore = useVideoClipStore()
+        //activityStore = useActivityStore()
+        //retrieveAllVideos = await videoStore.fetchVideoClips()
+        //retrieveSpecificVideo = await videoStore.fetchVideoClipById('637a61d49db11c5b4dd1b3b8')
+        wrapper = mount(AssignTimestamps, {
+            mocks: {
+                //$route
+            },
+            setup() {
+                var VideoClip = useVideoClipStore();
+                return VideoClip;
+            },
+            async mounted() {
+                var store = useUsersStore();
+                if (store.currentUserToken.length < 1) {
+                    this.$router.push({
+                        name: "LoginPage"
+                    })
+                }
+                await this.fetchVideoClips();
+                this.VideoClips = this.clips;
+                this.ready = true;
+            }
+        }as any)
+      })
+    it.only('renders videos in the selection list, equal to videoclips in db', () => {
+        const parentDiv = wrapper.find('div.video-list-div')
+        const childDivs = parentDiv.findAll('div.lesson')
+        expect(childDivs.length).toEqual(wrapper.vm.videoClips.length)
     })
-
-    
 })
