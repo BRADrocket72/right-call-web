4753ecb4fa9fa6db23bab4dcb0a6dedf337227a4
lleckron <leckmanbaseball@gmail.com>
2022-12-05 13:16:05 -0500

    simple test that verifies mock testing structure

--- web/tests/unit/AssignTimestamps.spec.ts
+++ web/tests/unit/AssignTimestamps.spec.ts
@@ -4,49 +4,28 @@ import { useVideoClipStore } from "@/stores/VideoClipStore"
 import { useActivityStore } from '@/stores/ActivityStore'
 import { useUsersStore } from '@/stores/UserStore'
 import AssignTimestamps from '@/components/AssignTimestamps.vue'
+import '@/routes.js'
 //import AssignTimestampsModal from '@/components/modals/AssignTimestampsModal.vue'
 //import AssignActivityModal from '@/components/modals/AssignActivityModal.vue'
 //import SaveTimestampsModal from '@/components/modals/SaveTimestampsModal.vue'
 import 'jest'
 
-describe('AssignTimestamps.vue', () => {
-    let wrapper: any
-    //let videoStore = null
-    //let activityStore = null
-    //let retrieveAllVideos: any
-    //let retrieveSpecificVideo: any
-    beforeAll(async () => {
-        setActivePinia(createPinia())
-        
-        // create an instance of the data stores
-        //videoStore = useVideoClipStore()
-        //activityStore = useActivityStore()
-        //retrieveAllVideos = await videoStore.fetchVideoClips()
-        //retrieveSpecificVideo = await videoStore.fetchVideoClipById('637a61d49db11c5b4dd1b3b8')
-        wrapper = mount(AssignTimestamps, {
+test('renders the selectable videos', async () => {
+    setActivePinia(createPinia())
+    const mockRoute = { }
+    const mockRouter = { 
+        push: jest.fn()
+    }
+
+    const wrapper = mount(AssignTimestamps, {
+        global: {
             mocks: {
-                //$route
-            },
-            setup() {
-                var VideoClip = useVideoClipStore();
-                return VideoClip;
-            },
-            async mounted() {
-                var store = useUsersStore();
-                if (store.currentUserToken.length < 1) {
-                    this.$router.push({
-                        name: "LoginPage"
-                    })
-                }
-                await this.fetchVideoClips();
-                this.VideoClips = this.clips;
-                this.ready = true;
+                $router: mockRouter,
+                $route: mockRoute
             }
-        }as any)
-      })
-    it.only('renders videos in the selection list, equal to videoclips in db', () => {
-        const parentDiv = wrapper.find('div.video-list-div')
-        const childDivs = parentDiv.findAll('div.lesson')
-        expect(childDivs.length).toEqual(wrapper.vm.videoClips.length)
+        },
     })
+    const parentDiv = wrapper.find('div.video-list-div')
+    const childDivs = parentDiv.findAll('div.lesson')
+    expect(childDivs.length).toEqual(wrapper.vm.videoClips.length)
 })
