cabe921cfe4365b13a141f920735747df00d28eb
lleckron <leckmanbaseball@gmail.com>
2022-12-07 15:41:50 -0500

    created tests for all clickable items in AssignTimestamps, refactor method name in AssignTimestamps

--- web/tests/unit/AssignTimestamps.spec.js
+++ web/tests/unit/AssignTimestamps.spec.js
@@ -21,13 +21,16 @@ describe('AssignTimestamps.vue', () => {
                 return {
                     ready: true,
                     videoClips: [{_id: "123test", videoUrl: "youtube.com", videoName: "Football Lesson One", timeStamps: [1,5,25]}, {_id: "123test", videoUrl: "youtube.com", videoName: "Football Lesson One", timeStamps: [1,5,25]}],
-                    isVideoSelected: false
+                    timestamps: [5,6],
+                    formattedTimestamps: [5,6],
+                    isVideoSelected: false,
+                    selectedVideo: {_id: "123test", videoUrl: "youtube.com", videoName: "Football Lesson One", timeStamps: [1,5,25], currentTime: 0}
                 }
             },
             global: {
                 mocks: {
                     $router: mockRouter,
-                    $route: mockRoute
+                    $route: mockRoute,
                 }
             }
         })
@@ -56,20 +59,57 @@ describe('AssignTimestamps.vue', () => {
     })
 
     it('renders the video editor when a lesson is selected', async () => {
-        const lesson = wrapper.find('a.nav-link')
+        const lesson = wrapper.find('div.lesson').find('a.nav-link')
         await lesson.trigger('click')
         const content = wrapper.find('div.main-content-div')
-        expect(content.exists()).toBeTruthy
+        expect(content.exists()).toBe(true)
     })
 
-    it('calls the toggleTimestampsModal when Add New Timestamp button is clicked', async () => {
-        const lesson = wrapper.find('a.nav-link')
+    it('calls the toggleTimestampsModal method when Add New Timestamp button is clicked', async () => {
+        const lesson = wrapper.find('div.lesson').find('a.nav-link')
         await lesson.trigger('click')
-        const addTimestampMock = jest.fn()
-        wrapper.vm.toggleTimestampsModal = addTimestampMock
-        const addTimestampButton = wrapper.find('#add-timestamp-button')
-        expect(addTimestampButton.exists()).toBeTruthy
-        //await addTimestampButton.trigger('click')
-        //expect(addTimestampMock).toHaveBeenCalled
+        wrapper.vm.toggleTimestampsModal = jest.fn()
+        const toggleModal = jest.spyOn(wrapper.vm, 'toggleTimestampsModal')
+        const addTimestamp = wrapper.find('.add-button-div').find('#add-timestamp-button')
+        expect(addTimestamp.exists()).toBe(true)
+        await addTimestamp.trigger('click')
+        expect(toggleModal).toHaveBeenCalled
+    })
+
+    it('calls the updateAPI method when Save button is clicked', async () => {
+        const lesson = wrapper.find('div.lesson').find('a.nav-link')
+        await lesson.trigger('click')
+        wrapper.vm.updateAPI = jest.fn()
+        const update = jest.spyOn(wrapper.vm, 'updateAPI')
+        const save = wrapper.find('.display-timestamps-div').find('#save-timestamps-button')
+        expect(save.exists()).toBe(true)
+        await save.trigger('click')
+        expect(update).toHaveBeenCalled
+    })
+
+    it('calls the toggleAssignActivityModal when Activity button is clicked', async () => {
+        const lesson = wrapper.find('div.lesson').find('a.nav-link')
+        await lesson.trigger('click')
+        wrapper.vm.toggleAssignActivityModal = jest.fn()
+        wrapper.vm.timestamps = [5,6]
+        wrapper.vm.formattedTimestamps = [5,6]
+        const activity = jest.spyOn(wrapper.vm, 'toggleAssignActivityModal')
+        const button = wrapper.find('.timestamps').find('ul').find('li').find('button#assign-activity-button')
+        expect(button.exists()).toBe(true)
+        await button.trigger('click')
+        expect(activity).toHaveBeenCalled
+    })
+
+    it('calls the deleteTimestamp method when Delete button is clicked', async() => {
+        const lesson = wrapper.find('div.lesson').find('a.nav-link')
+        await lesson.trigger('click')
+        wrapper.vm.deleteTimestamp = jest.fn()
+        wrapper.vm.timestamps = [5,6]
+        wrapper.vm.formattedTimestamps = [5,6]
+        const deleteTimestamp = jest.spyOn(wrapper.vm, 'deleteTimestamp')
+        const button = wrapper.find('.timestamps').find('ul').find('li').find('button#delete-timestamp-button')
+        expect(button.exists()).toBe(true)
+        await button.trigger('click')
+        expect(deleteTimestamp).toHaveBeenCalled
     })
 })
