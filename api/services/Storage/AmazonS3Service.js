103cbab17bc99d1c659d9ac9a650b41f5bae5edd
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2022-12-06 19:09:12 -0500

    removed excess bracket

--- api/services/Storage/AmazonS3Service.js
+++ api/services/Storage/AmazonS3Service.js
@@ -4,7 +4,7 @@ const uuid = require('uuid').v4
 
 exports.s3Upload = async (file) => {
     const s3Client = new S3Client()
-    const resourceName = `uploads/${uuid()}-${file.originalname}}`
+    const resourceName = `uploads/${uuid()}-${file.originalname}`
     const param = {
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: resourceName,
