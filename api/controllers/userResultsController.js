1cd7428e465891f2c9367308a0b649b6326c8260
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2023-01-17 14:10:12 -0500

    moved logic to contorller classes

---
+++ api/controllers/userResultsController.js
@@ -0,0 +1,41 @@
+const UserResults = require('../models/UserResults');
+
+exports.create_results = async (req, res) => {
+    const data = new UserResults({
+        username: req.body.username,
+        score: req.body.score,
+        lessonId: req.body.lessonId,
+        lessonName: req.body.lessonName
+    })
+
+    try {
+        res.header('Access-Control-Allow-Origin', '*')
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+exports.get_all = async (req, res) => {
+    try {
+        res.header('Access-Control-Allow-Origin', '*')
+        const data = await UserResults.find();
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+exports.get_by_username = async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    try {
+        const data = await UserResults.find({ "username": req.params.username });
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
