b8692c7566f261ee9d05b6dc20aabcf19637ab04
Lyle Dotson <ld77668952@gmail.com>
2023-01-31 13:06:20 -0500

    Create Instructor Page

---
+++ web/src/components/InstructorPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <LoggedInNavBar />
+  <br/><br/>
+  <div>
+    <h1>Instructor Page</h1>
+    <br><br>
+    <nav>
+        <router-link to="/CreateClassroomPage">Create Classroom Page</router-link> |
+        <router-link to="/AssignTimestamps">Assign Timestamps and Questions to a Video</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+import LoggedInNavBar from './LoggedInNavBar.vue';
+
+    
+export default {
+    name: "InstructorPage",
+    components: { LoggedInNavBar },
+    mounted() {
+    }
+}
+</script>
+
+<style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #4AAE9B;
+}
+
+</style>
