e96d92454dad1daba29c52a8feef246388b8a725
Lyle Dotson <ld77668952@gmail.com>
2023-01-31 13:27:16 -0500

    Create Create ClassRoomPage

---
+++ web/src/components/CreateClassroomPage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="CreateClassroomPage">
+        Hello World
+        <table>
+            <tr v-for="student in students" :key="student">
+                <a class="nav-link" @click="addStudentToClass(student._id)">
+                    {{student.userName}}
+                </a>
+            </tr>
+        </table>
+    </div>
+</template>
+
+
+<script>
+
+import { useUsersStore } from '@/stores/UserStore'
+
+export default {
+    name: 'CreateClassroomPage',
+    props: {
+        msg: String
+    },
+    data(){
+        return{students:[], 
+        allUsers:[]
+        }
+    },
+    async mounted(){
+        var users = useUsersStore();
+        this.allUsers = await users.getAllUsers();
+        this.students = this.retrieveOnlyStudents(this.allUsers);
+        console.log("students"+this.students);
+    },
+    methods:{
+        retrieveOnlyStudents(users){
+            var studentArray = [];
+            for (var user of users){
+                if (user.userType == "Student"){
+                    studentArray.push(user);
+                }
+            }
+            return studentArray;
+        },
+        addStudentToClass(type){
+            
+            return type
+        }
+    }
+}
+</script>
