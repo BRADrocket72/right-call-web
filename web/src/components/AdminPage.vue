f5019c86434cd8a3ebef89e8539e1704e7e4314b
Joshua Neumann <janeumann@bsu.edu>
2023-02-04 21:12:34 -0500

    added buttons to make it presentable

--- web/src/components/AdminPage.vue
+++ web/src/components/AdminPage.vue
@@ -5,9 +5,9 @@
     <h1>Admin Page</h1>
     <br><br>
     <nav>
-        <router-link to="/VideoUpload">Upload Lesson Video</router-link> |
-        <router-link to="/LessonDeletion">Delete Lesson</router-link> |
-        <router-link to="/AssignTimestamps">Assign Timestamps and Questions to a Video</router-link>
+      <button type="button"> <router-link to="/VideoUpload">Upload Lesson Video</router-link></button>  |
+      <button type="button"> <router-link to="/LessonDeletion">Delete Lesson</router-link> </button> |
+      <button type="button"> <router-link to="/AssignTimestamps">Assign Timestamps and Questions to a Video</router-link> </button>
     </nav>
   </div>
 </template>
@@ -32,6 +32,13 @@ nav {
 nav a {
   font-weight: bold;
   color: #4AAE9B;
+}
+
+.button {
+    width: 86px;
+    height: 40px;
+    border: 1px solid #0e333c;
+    background: #FFFFFF;
 }
 
 </style>
