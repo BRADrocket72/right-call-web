6f89563d18ebaa36ef0068647d1c0cec71498155
lleckron <leckmanbaseball@gmail.com>
2023-02-05 22:13:13 -0500

    add eyeTrackingFeedback tests

---
+++ web/tests/unit/EyeTrackingFeedbackModal.spec.ts
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils'
+import EyeTrackingFeedbackModal from '@/components/modals/EyeTrackingFeedbackModal.vue'
+import 'jest'
+
+describe('EyeTrackingFeedbackModal.vue', () => {
+    let wrapper: any;
+    beforeEach(() => {
+        wrapper = mount(EyeTrackingFeedbackModal, {
+            props:{
+                answer: 'Correct',
+                correctAnswer: 'quadrant-one'
+            }
+        })
+    })
+
+    it('renders EyeTrackingFeedbackModal', () => {
+        expect(wrapper.exists()).toBe(true)
+    })
+
+    it('renders the proper div for a Correct answer', () => {
+        const div = wrapper.find('.feedback-div').find('h2').text()
+        expect(div).toBe('Nice Job!')
+    })
+
+    it('renders the proper div for an Incorrect answer', () => {
+        wrapper = mount(EyeTrackingFeedbackModal, {
+            props:{
+                answer: 'Incorrect',
+                correctAnswer: 'quadrant-two'
+            }
+        })
+        const div = wrapper.find('.feedback-div').find('h2').text()
+        expect(div).toBe('Almost')
+    })
+})
