5f556fc3b31b60fc83c992e3384687948babc78d
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-02-05 21:21:52 -0500

    one more test

---
+++ web/tests/unit/AddVideosToClassModal.spec.js
@@ -0,0 +1,48 @@
+import { mount } from '@vue/test-utils'
+import { setActivePinia, createPinia } from 'pinia'
+import InstructorClassDto  from '@/models/InstructorClassDto'
+import 'jest'
+import AddVideosToClassModal from '@/components/modals/AddVideosToClassModal.vue';
+
+describe('AssignActivityModal.vue', () => {
+    let wrapper;
+    beforeEach(() => {
+        setActivePinia(createPinia())
+        wrapper = mount(AddVideosToClassModal, {
+            props:{
+                selectedClass: [new InstructorClassDto("test123", "Test Class", ["123","1234"], ["student1", "student2"])]
+            },
+            data() {
+                return{
+                    lessons: [{_id: "testVideo123", videoName: "testVideo123"}]
+                }
+            }
+        })
+    })
+
+    it('renders AssignActivityModal', () => {
+        expect(wrapper.exists()).toBe(true)
+    })
+
+    it('displays the header div', () => {
+        const modalHeader = wrapper.find('.modal-header')
+        expect(modalHeader.exists()).toBeTruthy
+    })
+
+    it('Contains the two containers of Students', () => {
+        const studentTables = wrapper.findAll('.flex-container')
+        expect(studentTables.length).toEqual(2)
+    })
+    it('Displays the correct amount of currently added videos to remove and add', () => {
+        const notAddedVideos = wrapper.findAll('.addVideo')
+        expect(notAddedVideos.length).toEqual(1)
+        const addedVideos = wrapper.findAll('.removeVideo')
+        expect(addedVideos.length).toEqual(0)
+    })
+
+     it('Displays the close button', () => {
+        const closeButton = wrapper.find('.btn-green')
+        expect(closeButton.exists).toBeTruthy()
+    })
+    
+})
