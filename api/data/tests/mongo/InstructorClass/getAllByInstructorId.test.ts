98d5472e64bc4f0373d008c9f19a82620f256d15
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2023-02-18 16:12:54 -0500

    Added a few tests

---
+++ api/data/tests/mongo/InstructorClass/getAllByInstructorId.test.ts
@@ -0,0 +1,40 @@
+import InstructorClass from '../../../mongo/InstructorClass'
+import InstructorClassSchema from '../../../mongo/schemas/InstructorClassSchema'
+
+
+const testMongoDb = require('../testMongoDb')
+const instructorClassCollection :InstructorClass =  new InstructorClass()
+
+beforeAll(async () => await testMongoDb.connect())
+
+beforeEach(async () => {
+    const instructorClassOne =new InstructorClassSchema({instructorId:"instructor1",className:"Basketball",videoclipIds:["id1","id2"],studentIds:["student1","student2"]})
+    const instructorClassTwo =new InstructorClassSchema({instructorId:"instructor1",className:"Basketball",videoclipIds:["id3","id4"],studentIds:["student3","student4"]})
+    const instructorClassThree =new InstructorClassSchema({instructorId:"instructor3",className:"Basketball",videoclipIds:["id5","id6"],studentIds:["student5","student6"]})
+    await instructorClassOne.save();
+    await instructorClassTwo.save();
+    await instructorClassThree.save();
+})
+
+afterEach(async () => await testMongoDb.clearDatabase())
+
+afterAll(async () => await testMongoDb.closeDatabase())
+
+
+describe('Get all InstructorClasses',()=>
+{
+    it('get Two Ids',()=>{
+    const instructorClassResult = await instructorClassCollection.getAllByInstructorId("instructor1")
+    expect(instructorClassResult.length).toEqual(2);
+    expect(instructorClassResult[0].instructorId).toEqual("instructor1")
+    expect(instructorClassResult[1].instructorId).toEqual("instructor1")
+    })
+
+    it('get One Id',()=>{
+        const instructorClassResult = await instructorClassCollection.getAllByInstructorId("instructor3")
+        expect(instructorClassResult.length).toEqual(1);
+        expect(instructorClassResult[0].instructorId).toEqual("instructor3")
+        })
+
+
+})
