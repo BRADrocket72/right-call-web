0a211c347aebc1515e3933ed7909d5a6f2c22b74
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-02-24 19:08:00 -0500

    add lesson customization pages

---
+++ web/src/components/UpdateInstructorLessonsPage.vue
@@ -0,0 +1,201 @@
+<template>
+    <div>
+      <LoggedInNavBar />
+      <div class="admin-container">
+        <br/>
+        <h1>Select Admin Lesson to Customize</h1>
+        <div class="admin-buttons">
+            <div v-for="lesson in allLessons" :key="lesson._id">
+                <!-- <button type="button" class="button-selection buttonLink" @click="selectLessonToCustomize(lesson.name, lesson.videoClipsArray)"><span>{{lesson.name}}</span></button> -->
+                <button type="button" class="button-selection buttonLink" @click="confirmCustomization(lesson)"><span>{{lesson.name}}</span></button>
+                <p class="upload-description">{{lesson.description}}<br/><br/>Content: {{ lesson.videoClipsArray.length }} videos</p>
+            </div>
+        </div>
+      </div>
+      <LessonCustomizationConfirmation v-if="isCustomizationConfirmed" :lesson="selectedLesson" @close="closeModal"/>
+    </div>
+    </template>
+    
+    <script>
+    import LoggedInNavBar from './LoggedInNavBar.vue';
+    import { useLessonStore } from '@/stores/LessonsStore';
+    import LessonCustomizationConfirmation from '@/components/modals/LessonCustomizationConfirmation.vue';
+    import { useVideoClipStore } from "@/stores/VideoClipStore";
+    import { useUsersStore } from "@/stores/UserStore";
+    import { useInstructorLessonStore } from "@/stores/InstructorLessonStore";
+    
+        
+    export default {
+        name: "UpdateInstructorLessonsPage",
+        components: { 
+          LoggedInNavBar,
+          LessonCustomizationConfirmation
+        },
+        data() {
+            return {
+                allLessons: [],
+                isCustomizationConfirmed: false,
+                uploadedInstructorVideos: [],
+                selectedLesson: ""
+            }
+        },
+        async mounted() {
+            let lessonStore = useLessonStore()
+            this.allLessons = await lessonStore.getAllLessons()
+        },
+        methods: {
+            selectLessonToCustomize(lessonName, lessonArrayOfVideos) {
+                let lessonPack = [lessonName, lessonArrayOfVideos]
+                this.$router.push({
+                    name: "CustomizeLesson",
+                    params: {
+                        lessonPack: JSON.stringify(lessonPack)
+                    }
+                })
+            },
+            confirmCustomization(lesson) {
+              this.isCustomizationConfirmed = true
+              this.selectedLesson = lesson
+            },
+            async closeModal(isLessonConfirmed, lesson) {
+              if (isLessonConfirmed == "true") {
+                this.isCustomizationConfirmed = false
+                let videoClipStore = useVideoClipStore()
+                for (let i=0; i<lesson.videoClipsArray.length; i++) {
+                  this.uploadedInstructorVideos.push(await videoClipStore.postInstructorsCustomizedVideo(lesson.videoClipsArray[i].videoName, lesson.videoClipsArray[i].videoURL))
+                }
+                let instructorLessonStore = useInstructorLessonStore();
+                var userStore = useUsersStore();
+                let instructorUsername = this.$cookies.get("user_session").currentUserName
+                let instructor =  await userStore.getUserByName(instructorUsername)
+                this.instructorId = instructor._id
+                await instructorLessonStore.postInstructorLesson(lesson.name, this.instructorId, lesson.description, this.uploadedInstructorVideos)
+                this.$router.push({
+                    name: "InstructorPage"
+                })
+              } else {
+                this.isCustomizationConfirmed = false
+              }
+            },
+
+        }
+    }
+    </script>
+    
+    <style>
+    
+    .button-selection {
+      display: inline-flex;
+      align-content: flex-start;
+      flex: 1 0 0;
+      height: 300px;
+      width: 300px;
+      margin: auto 20px auto;
+      border: 4px solid #0e333c;
+      color: #0e333c;
+      background: #e9e9e9;
+      border-radius: 10px;
+      box-shadow: 5px 5px 10px #d3d3d3;
+    }
+    
+    .button-selection span {
+      margin: 20px auto;
+      font-size: 25px;
+      font-weight: bold;
+    }
+    
+    .upload-description {
+      visibility: hidden;
+      opacity: 0;
+      position: absolute;
+      max-width: 250px;
+      margin: -150px 0 0 42px;
+      font-size: 19px;
+      font-weight: normal;
+      color: #0e333c;
+      transition: visibility 0s, opacity 0.3s linear;
+    }
+    
+    .upload-description:hover {
+      visibility: visible;
+      opacity: 1;
+    }
+    
+    .button-selection:hover + .upload-description {
+      visibility: visible;
+      opacity: 1;
+    }
+    
+    .delete-description {
+      visibility: hidden;
+      opacity: 0;
+      position: absolute;
+      max-width: 250px;
+      margin: -150px 0 0 42px;
+      font-size: 19px;
+      font-weight: normal;
+      color: #0e333c;
+      transition: visibility 0s, opacity 0.3s linear;
+    }
+    
+    .delete-description:hover {
+      visibility: visible;
+      opacity: 1;
+    }
+    
+    .button-selection:hover + .delete-description {
+      visibility: visible;
+      opacity: 1;
+    }
+    
+    .assign-description {
+      visibility: hidden;
+      opacity: 0;
+      position: absolute;
+      max-width: 250px;
+      margin: -150px 0 0 42px;
+      font-size: 19px;
+      font-weight: normal;
+      color: #0e333c;
+      transition: visibility 0s, opacity 0.3s linear;
+    }
+    
+    .assign-description:hover {
+      visibility: visible;
+      opacity: 1;
+    }
+    
+    .button-selection:hover + .assign-description {
+      visibility: visible;
+      opacity: 1;
+    }
+    
+    .admin-buttons {
+      display: flex;
+      margin-top: 60px;
+    }
+    
+    .admin-buttons a {
+      font-weight: bold;
+      color: #4AAE9B;
+      text-decoration: none;
+    }
+    
+    @media only screen and (min-width: 1400px) {
+      .admin-buttons {
+        margin-left: 175px;
+      }
+    }
+    
+    @media only screen and (min-width: 1200px) and (max-width: 1399px){
+      .admin-buttons {
+        margin-left: 100px;
+      }
+    }
+    
+    @media only screen and (max-width: 1199px){
+      .admin-buttons {
+        margin-left: 0;
+      }
+    }
+    </style>
---
+++ web/src/components/modals/LessonCustomizationConfirmation.vue
@@ -0,0 +1,130 @@
+<template>
+    <transition name="modal-fade">
+      <div class="modal-backdrop">
+        <div class="modal" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
+          <header class="modal-header" id="modalTitle">
+            <slot name="header">
+              Create Custom Lesson Confirmation
+            </slot>
+          </header>
+  
+          <section class="modal-body" id="modalDescription">
+            <slot name="body">
+              <br>
+              <p>Do you want to create your own version of this lesson?</p>
+  
+              <button type="button" class="btn-green" @click="close('true')">  Yes </button>
+              <button type="button" class="btn-green" @click="close('false')">  Exit </button>
+            </slot>
+          </section>
+        </div>
+      </div>
+    </transition>
+  </template>
+  
+  <script>  
+  export default {
+    name: 'ResultsPage',
+    data() {
+      return {
+        numberOfQuestions: 0,
+        percentageCorrect: "",
+        isLessonConfirmed: "",
+        newLesson: ""
+      }
+    },
+    props: {
+      lesson: Object      
+    },
+    mounted() {
+      this.newLesson = this.lesson
+    },
+    methods: {
+      close(boolean) {
+        this.isLessonConfirmed = boolean
+        this.$emit('close', this.isLessonConfirmed, this.newLesson)
+      },
+      
+    }
+  };
+  </script>
+  
+  <style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .modal {
+    background: white;
+    width: 600px;
+    height: 300px;
+    border-radius: 2px;
+    position: fixed;
+    left: auto;
+    top: auto;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .modal-header {
+    position: relative;
+    border-bottom: 1px solid #eeeeee;
+    background-color: #4AAE9B;
+    justify-content: space-between;
+    border-radius: 2px 2px 0px 0px;
+  
+  }
+  
+  .modal-footer {
+    border-top: 1px solid #eeeeee;
+    flex-direction: column;
+  }
+  
+  .modal-body {
+    position: relative;
+    background: white;
+    padding: 20px 10px;
+  }
+  
+  .btn-close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    border: none;
+    font-size: 20px;
+    padding: 10px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+  
+  .btn-green {
+    color: white;
+    background: #4AAE9B;
+    border: 1px solid #eeeeee;
+    border-radius: 2px;
+    padding: 15px;
+    margin: 10px;
+  }
+  
+  .modal-fade-enter,
+  .modal-fade-leave-to {
+    opacity: 0;
+  }
+  
+  .modal-fade-enter-active,
+  .modal-fade-leave-active {
+    transition: opacity .5s ease;
+  }
+  </style>
