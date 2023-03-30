baf42849ddb0f33810bcece13a0926bb439cb4b9
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-03-30 12:30:41 -0400

    update router functions

--- api/webApi/controllers/instructorClassController.ts
+++ api/webApi/controllers/instructorClassController.ts
@@ -32,16 +32,6 @@ const instructorClassDb = new InstructorClass();
             res.status(500).json({ message: error.message })
         }
     }
-    module.exports.delete_video_clip = async(req, res) => {
-        try {
-            res.header('Access-Control-Allow-Origin', '*')
-            const data = await instructorClassDb.deleteVideoClip(req.params.classId, req.body);
-            res.json(data)
-        }
-        catch (error) {
-            res.status(500).json({ message: error.message })
-        }
-    }
     module.exports.add_video_clip = async(req, res) => {
         try {
             res.header('Access-Control-Allow-Origin', '*')
@@ -56,6 +46,16 @@ const instructorClassDb = new InstructorClass();
         try {
             res.header('Access-Control-Allow-Origin', '*')
             const data = await instructorClassDb.updateStudentIdList(req.params.classId, req.body);
+            res.json(data)
+        }
+        catch (error) {
+            res.status(500).json({ message: error.message })
+        }
+    }
+    module.exports.get_by_classId = async(req, res) => {
+        try {
+            res.header('Access-Control-Allow-Origin', '*')
+            const data = await instructorClassDb.getByClassId(req.params.classId);
             res.json(data)
         }
         catch (error) {
