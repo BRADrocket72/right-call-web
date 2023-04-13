a00bb00af5044fd183d8abf733ad2bbee5198aad
Bradley Vaal <67610940+BRADrocket72@users.noreply.github.com>
2023-04-13 12:25:13 -0400

    added feedback back

---
+++ api/webApi/routes/feedbackRouter.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+const feedbackController = require('../controllers/feedbackController.ts')
+const router = express.Router()
+//Post Method
+router.post('/feedback/post', feedbackController.feedback_create_feedback)
+//Get all Method
+router.get('/feedback/getAll', feedbackController.feedback_get_all)
+//Get by VideoClip Method
+router.get('/feedback/get/:videoclipId', feedbackController.feedback_get_by_videoId)
+//Get by ActivityId Method
+router.get('/feedback/getByActivity/:activityId', feedbackController.feedback_get_by_activityId)
+//Update by ID Method
+router.put('/feedback/update/:id', feedbackController.feedback_update)
+router.delete('/feedback/delete/:id', feedbackController.feedback_delete_feedback)
+module.exports = router;
