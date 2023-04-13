963376b723e96276370eb6d4cb3299f83d47aa3d
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-04-13 14:01:49 -0400

    update webgazer

--- web/src/components/VideoEditor.vue
+++ web/src/components/VideoEditor.vue
@@ -141,17 +141,29 @@
         }
         this.permissionModalVisible = false
       },
-      webgazerSetup() {
+      async webgazerSetup() {
         let cookiesCalibration = this.$cookies.get("user_session").currentEyeTrackingCalibration
         if (cookiesCalibration == "false" || !webgazer.isReady()) {
           this.calibrationReady = true
           this.replaceCookie()
-          webgazer.applyKalmanFilter(true)
-          webgazer.showVideo(false)
-          webgazer.showFaceOverlay(false)
-          webgazer.showFaceFeedbackBox(false)
-          webgazer.showPredictionPoints(true)
-          webgazer.begin()
+          // webgazer.applyKalmanFilter(true)
+          // webgazer.setRegression("ridge")
+          // webgazer.showVideo(false)
+          // webgazer.showFaceOverlay(false)
+          // webgazer.showFaceFeedbackBox(false)
+          // webgazer.showPredictionPoints(true)
+          // webgazer.begin()
+          await webgazer.setRegression('ridge') /* currently must set regression and tracker */
+            .setGazeListener(function() {
+                
+            })
+            .begin();
+          webgazer.showVideoPreview(false) /* shows all video previews */
+            .showVideo(false)
+            .showFaceOverlay(false)
+            .showFaceFeedbackBox(false)
+            .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
+            .applyKalmanFilter(true); 
         }
         else {
           webgazer.showPredictionPoints(true)
